486ae6a15a06f3e73d60d5c091d43e2ff257f344
Jeremy Rossi <jeremy@jeremyrossi.com>
2014-10-29 21:54:53 -0400

    alskdja

--- start.sh
+++ start.sh
@@ -13,8 +13,10 @@ echo "initLimit=5" >> /opt/zookeeper/conf/zoo.cfg
 echo "syncLimit=2" >> /opt/zookeeper/conf/zoo.cfg 
 for i in ${!ZOOKEEPER_IP_*}; do 
     x=`echo $i | sed "s/.*_IP_//"`
-    echo "server.${x}=${i}:2888:3888" >> /opt/zookeeper/conf/zoo.cfg
+
+    echo "server.${x}=${$i}:2888:3888" >> /opt/zookeeper/conf/zoo.cfg
 done
+cat /opt/zookeeper/conf/zoo.cfg
 
 
 exec /opt/zookeeper/bin/zkServer.sh $@
