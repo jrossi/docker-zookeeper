665ef6000002d8e4b07042b11818627826acdb9c
Jeremy Rossi <jeremy@jeremyrossi.com>
2014-10-29 22:22:07 -0400

    alksjda

--- start.sh
+++ start.sh
@@ -11,10 +11,11 @@ echo "dataDir=/var/zookeeper" >> /opt/zookeeper/conf/zoo.cfg
 echo "clientPort=2181" >> /opt/zookeeper/conf/zoo.cfg
 echo "initLimit=5" >> /opt/zookeeper/conf/zoo.cfg
 echo "syncLimit=2" >> /opt/zookeeper/conf/zoo.cfg 
-for i in ${!ZOOKEEPER_IP_*}; do 
-    x=`echo $i | sed "s/.*_IP_//"`
-
-    echo "server.${x}=${$i}:2888:3888" >> /opt/zookeeper/conf/zoo.cfg
+for i in `echo "ZOOKEEPER_MAPPING" | sed "s/,/ /g"`
+do
+  n=`echo $i | cut -d":" -f 1`
+  h=`echo $i | cut -d":" -f 2`
+  echo "server.${n}=${h}:2888:3888" >> /opt/zookeeper/conf/zoo.cfg
 done
 cat /opt/zookeeper/conf/zoo.cfg
 
